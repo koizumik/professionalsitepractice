66cc8c3dc4e082386ce181cad792ecb6f2aaa146
keenan koizumi <keenankoizumi@gmail.com>
2017-03-02 22:21:21 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// $(document).ready(function() {
+// 	$("button").click(function(){
+// 		$(".one, .two, .three").animate({
+// 			left: "300px",
+// 			width: "500px",
+// 			height: "400px",
+// 		});	
+// 	});
+// });
+
+$(document).ready(function(){
+	$("button").click(function(){
+		$("#list").append("<li>five</li>");
+	});
+});
+
+// installed a plugin
+// this js will make it snow filling the entire viewport
+
+// $("#divSnow").websnowjq();
+
+// ==these are examples of an object (1), method(2), and function (3)
+// 1. var person = {
+// 	name: "James",
+// 	age: 23,
+
+// 2. agePlusTwo: function(){
+
+// 	}
+// };
+
+// 3. function times2(){
+
+// }
